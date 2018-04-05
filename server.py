b8b23a833d7b8d21ad3884499da7139e7bbb8913
BiggieBoo18 <ns182418@gmail.com>
2018-04-05 16:31:33 +0900

    improve client server

--- server.py
+++ server.py
@@ -29,6 +29,12 @@ class Server:
         client.writer.write('{}\n'.format(msg).encode())
         return
 
+    def send_to_group(self, names, msg):
+        print('Got message "{}", send to all clients'.format(msg))
+        for name in names:
+            self.send_to_client(name, msg)
+        return
+
     def send_to_all_clients(self, peername, msg):
         print('Got message "{}", send to all clients'.format(msg))
         for client_peername, client in self.clients.items():
@@ -41,9 +47,6 @@ class Server:
         for peername, client in self.clients.items():
             client.writer.write_eof()
 
-    def receive_private_msg(self, msg):
-        pass
-
     def register_user(self, username, client):
         if not self.clients.get(username):
             self.clients[username] = client
@@ -51,12 +54,45 @@ class Server:
         else:
             return False
 
+    def execute_command(self, peername, new_client, msg, username):
+        part_msg = msg.split(" ")
+        if part_msg[0]=="join": # join command
+            if len(part_msg)>1: # "join username"
+                if self.register_user(part_msg[1], new_client):
+                    username = part_msg[1]
+                    self.send_to_client(part_msg[1], 'Registered: {}'.format(part_msg[1]))
+                    return username
+                else:
+                    self.send_to_client(part_msg[1], '[Error]Already used: {}'.format(part_msg[1]))
+            else:
+                self.send_to_client(peername, '[Error]Invalid command: {}'.format(msg))
+        elif part_msg[0]=="send": # send command
+            if len(part_msg)>2: # "send username/groupname message"
+                send_to_addr = self.clients.get(part_msg[1])
+                if send_to_addr:
+                    self.send_to_client(part_msg[1], "{} >>> {}".format(username, " ".join(part_msg[2:])))
+                else:
+                    send_to_grp = self.groups.get(part_msg[1])
+                    if send_to_grp:
+                        self.send_to_group(send_to_grp, "{} >>> {}".format(username, " ".join(part_msg[2:])))
+                    else:
+                        self.send_to_client(username, "[Error]Username or Groupname is not found: {}".format(msg))
+            else:
+                self.send_to_client(peername, '[Error]Invalid command: {}'.format(msg))
+        elif part_msg[0]=="create": # create command
+            if len(part_msg)>2: # "create groupname user1 user2..."
+                groupname = part_msg[1]
+                self.groups[groupname] = part_msg[2:]
+                self.send_to_group(part_msg[2:], 'Created group: {}'.format(part_msg[1]))
+            else:
+                self.send_to_client(peername, '[Error]Invalid command: {}'.format(msg))
+
     @asyncio.coroutine
     def client_connected(self, reader, writer):
         print('Client connected.')
         peername = writer.transport.get_extra_info('peername')
         new_client = Client(reader, writer)
-        # self.clients[peername] = new_client
+        self.clients[peername] = new_client
         # self.send_to_client(peername, 'Welcome to this server client: {}'.format(peername))
         username = ""
         while not reader.at_eof():
@@ -67,25 +103,9 @@ class Server:
                     print('Server Received: "{}"'.format(msg))
                     if not msg == 'close()':
                         # parse command
-                        part_msg = msg.split(" ")
-                        if part_msg[0]=="join": # join command
-                            if len(part_msg)>1:
-                                if self.register_user(part_msg[1], new_client):
-                                    username = part_msg[1]
-                                    self.send_to_client(part_msg[1], 'Registered: {}'.format(part_msg[1]))
-                                else:
-                                    self.send_to_client(part_msg[1], '[Error]Already used: {}'.format(part_msg[1]))
-                            else:
-                                self.send_to_client(peername, '[Error]Invalid command: {}'.format(msg))
-                        elif part_msg[0]=="send": # send command
-                            try:
-                                self.clients[part_msg[1]]
-                            except KeyError:
-                                self.send_to_client(username, "[Error]Username is not found: {}".format(msg))
-                            else:
-                                self.send_to_client(part_msg[1], "{} >>> {}".format(username, " ".join(part_msg[2:])))
-                        elif part_msg[0]=="create": # create command
-                            pass
+                        ret = self.execute_command(peername, new_client, msg, username)
+                        if ret:
+                            username = ret
                     else:
                         print('User {} disconnected'.format(username))
                         try:
